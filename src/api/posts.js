32db0e75cb44899005a10916254d363d2286141e
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-08-15 02:04:27 +0100

    api call to return all posts

---
+++ src/api/posts.js
@@ -0,0 +1,19 @@
+const getposts = async (token) => {
+  const response = await fetch("http://localhost:3000/api/posts", {
+    credentials: "include",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  if (!response.ok) {
+    const error = new Error("Failed to fetch posts");
+    error.status = response.status;
+    throw error;
+  }
+  
+  const posts = await response.json();  // parses it into a JavaScript object 
+  return posts;
+};
+
+export default getposts;
