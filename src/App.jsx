44ab3304afaf0ed80fdbb45625c11b90fbaa6df1
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-08-15 23:52:05 +0100

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Navigate, BrowserRouter, Routes, Route } from "react-router";
+
 import Homepage from "./pages/homepage";
 import BlogPage from "./pages/blogPage";
-import Error from "./pages/error";
-import { BrowserRouter, Routes, Route } from "react-router";
+import LoginPage from "./pages/loginPage";
+import SignupPage from "./pages/signupPage";
+import Error from "./pages/Error";
+
+import { useAuthContext } from "./context/useAuthContext";
+
+
+const queryClient = new QueryClient();
 
 function App() {
+
+  const { isAuthenticated } = useAuthContext();
+
+
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/pages/blog/:id" element={<BlogPage />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            { isAuthenticated ? (
+              <>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/pages/posts/:postId" element={<BlogPage />} />
+                <Route path="*" element={<Error />} />
+              </>
+            ) : (
+              <>
+              <Route path="/api/auth/log-in" element={<LoginPage />} />
+              <Route path="/api/auth/sign-up" element={<SignupPage />} />
+              <Route path="*" element={<Navigate to="/api/auth/log-in" />} />     { /*Redirect all other paths to log-in if not authenticated */}
+              </>
+            ) }
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
   );
 }
 
 export default App;
+
