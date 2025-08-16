de93b5a269dadc801047f73605c9d99fe08dae1d
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-08-16 01:50:53 +0100

    api call to update a comment

---
+++ src/api/updateComment.js
@@ -0,0 +1,25 @@
+const updateComment = async (token, postId, commentId, updatedContent) => {
+  const response = await fetch(`http://localhost:3000/api/posts/${postId}/comments/${commentId}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ updatedContent })    // converts to JSON string
+  })
+
+
+  const data = await response.json(); // parses it into a JavaScript object 
+
+  return {
+    status: response.status,
+    data    
+  };
+
+}
+
+
+
+
+export default updateComment
