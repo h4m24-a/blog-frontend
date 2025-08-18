6fbb819a67d9ce187fff96bc5c4f97b3a7eb8977
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-08-18 23:22:09 +0100

    homepage to display all blog posts

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,183 +1,105 @@
 import Navbar from "../components/navbar";
+import { Link } from "react-router";
 import Footer from "../components/footer";
-const posts = [
-  {
-    id: 1,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    id: 2,
-    title: "Design systems that scale",
-    href: "#",
-    description:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    date: "Apr 22, 2020",
-    datetime: "2020-04-22",
-    category: { title: "Design", href: "#" },
-    author: {
-      name: "Lindsay Walton",
-      role: "Product Designer",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    id: 3,
-    title: "Data-driven decision making",
-    href: "#",
-    description:
-      "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.",
-    date: "May 10, 2020",
-    datetime: "2020-05-10",
-    category: { title: "Data Science", href: "#" },
-    author: {
-      name: "Tom Cook",
-      role: "Director of Data",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    id: 4,
-    title: "Building inclusive teams",
-    href: "#",
-    description:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    date: "Jun 5, 2020",
-    datetime: "2020-06-05",
-    category: { title: "HR", href: "#" },
-    author: {
-      name: "Courtney Henry",
-      role: "People Operations Manager",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-    {
-    id: 4,
-    title: "Building inclusive teams",
-    href: "#",
-    description:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    date: "Jun 5, 2020",
-    datetime: "2020-06-05",
-    category: { title: "HR", href: "#" },
-    author: {
-      name: "Courtney Henry",
-      role: "People Operations Manager",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-    {
-    id: 4,
-    title: "Building inclusive teams",
-    href: "#",
-    description:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    date: "Jun 5, 2020",
-    datetime: "2020-06-05",
-    category: { title: "HR", href: "#" },
-    author: {
-      name: "Courtney Henry",
-      role: "People Operations Manager",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  
-];
+import { useQuery } from "@tanstack/react-query";
+import getposts from "../api/posts";
+import { useAuthContext } from "../context/useAuthContext";
+
 
 export default function Homepage() {
 
 
+  const { accessToken } = useAuthContext();
 
 
+  const { data: posts, isLoading, isError, error } = useQuery( {
+    queryKey: ["posts"],
+    queryFn: () => getposts(accessToken),   // token from auth context global state
+    enabled: !!accessToken
+  });
 
 
+  if (isLoading) {
+    return <div>Loading posts...</div>;
+  }
+
+ 
 
 
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+
+  
+  
 
   return (
-    <>
-    <Navbar/>
-      <div className=" bg-white sm: py-12">
+  <>
+      <Navbar />
+      <main>
+      <div className="bg-white sm:py-12">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-auto text-center">
-            <h2 className="text-4xl font-semibold font-mono tracking-tight text-pretty text-gray-900 sm:text-5xl">
+          <div className="mx-auto max-w-fit text-center">
+            <h2 className="text-4xl font-semibold font-mono tracking-tight text-gray-900 sm:text-5xl">
               From the blog
             </h2>
-            <p className="mt-2 text-lg/8 font-mono text-gray-600">
+            <p className="mt-2 text-lg font-mono text-gray-600">
               This is where I share what I’ve learned about life, growth, and everything in between.
             </p>
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
-              <article
+
+          {posts?.length === 0 &&  (
+            <p className=" text-xl text-center mt-6 text-red-500 font-mono">No posts are found!</p>
+            )}
+
+
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:max-w-none lg:grid-cols-3">
+            {posts?.map((post) => (
+              <Link to={`/pages/posts/${post.id}`}
+              
                 key={post.id}
                 className="flex max-w-xl bg-gray-100 py-3 px-4 rounded-2xl flex-col items-start justify-center"
               >
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
+                <div className="flex flex-col items-start gap-x-4 text-xs text-gray-500">
+                  <time dateTime={post.createdAt}>
+                    {new Date(post.createdAt).toLocaleDateString("en-gb", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric"
+                    })}
                   </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                  </a>
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
+
+
+
+                <div className="flex flex-col">
+                  <h3 className="mt-3 line-clamp-1 text-lg font-semibold cursor-pointer text-gray-900 group-hover:text-gray-600">
+                    {post.title}
                   </h3>
-                  <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">
-                    {post.description}
+                  <p className="mt-5 line-clamp-1 text-sm text-gray-600">
+                    {post.content}
                   </p>
                 </div>
+
                 <div className="relative mt-8 flex items-center gap-x-4">
-                  <img
-                    alt=""
-                    src={post.author.imageUrl}
-                    className="size-10 rounded-full bg-gray-50"
-                  />
-                  <div className="text-sm/6">
-                    <p className="font-semibold text-gray-900">
-                      <a href={post.author.href}>
-                        <span className="absolute inset-0" />
-                        {post.author.name}
-                      </a>
-                    </p>
-                    <p className="text-gray-600">{post.author.role}</p>
+                  <img src="https://plus.unsplash.com/premium_photo-1723708857381-82e3b34187b5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aGVhZHNob3R8ZW58MHwyfDB8fHww" alt="" className="size-10 rounded-full bg-gray-50" />
+                  <div className="text-sm">
+                    <p className="font-semibold text-gray-900">{post.author.username}</p>
+                  <p className="text-gray-600">Co-Founder / Admin</p>
                   </div>
                 </div>
-              </article>
+
+                </div>
+
+              </Link>
             ))}
           </div>
         </div>
       </div>
+        
+      </main>
       <Footer />
     </>
   );
 }
+
