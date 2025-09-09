35bdf2de710d182ff6b7ca310a391ca36c689624
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-09-10 00:09:32 +0100

    added global loading state for authentication and routing for admin users

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Homepage from "./pages/homepage";
 import BlogPage from "./pages/blogPage";
 import LoginPage from "./pages/loginPage";
 import SignupPage from "./pages/signupPage";
+import Dashboard from "./pages/admin/dashboard";
+import AdminBlogPage from "./pages/admin/adminBlogPage";
+import AdminHomePage from "./pages/admin/adminHomepage";
 import Error from "./pages/Error";
+import NotAuthorized from "./pages/notAuthorized";
 
 import { useAuthContext } from "./context/useAuthContext";
 
@@ -14,30 +18,55 @@ const queryClient = new QueryClient();
 
 function App() {
 
-  const { isAuthenticated } = useAuthContext();
+  const { isAuthenticated, role, loading } = useAuthContext();
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
 
 
   return (
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            { isAuthenticated ? (
-              <>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/pages/posts/:postId" element={<BlogPage />} />
-                <Route path="*" element={<Error />} />
-              </>
-            ) : (
-              <>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {isAuthenticated ? (
+            <>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/pages/posts/:postId" element={<BlogPage />} />
+
+              {role === "ADMIN" && (
+                <>
+                  <Route path="/pages/admin/dashboard" element={<Dashboard />}  />
+                  <Route path="/pages/admin/posts" element={<AdminHomePage />} />
+                  <Route path="/pages/admin/posts/:postId" element={<AdminBlogPage />} />
+                </>
+              )}
+
+              {role === "USER" && (
+                <>
+                <Route path="/pages/admin/dashboard" element={<NotAuthorized />}  />
+                <Route path="/pages/admin/posts" element={<NotAuthorized />} />
+                <Route path="/pages/admin/posts/:postId" element={<NotAuthorized />} />
+                </>
+              )}
+
+
+
+
+              <Route path="*" element={<Error />} />
+            </>
+
+          ) : (
+            <>
               <Route path="/api/auth/log-in" element={<LoginPage />} />
               <Route path="/api/auth/sign-up" element={<SignupPage />} />
-              <Route path="*" element={<Navigate to="/api/auth/log-in" />} />     { /*Redirect all other paths to log-in if not authenticated */}
-              </>
-            ) }
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
+              <Route path="*" element={<Navigate to="/api/auth/log-in" />}/> {" "} {/*Redirect all other paths to log-in if not authenticated */}
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
